f337b6c6cc2fb43e41768f1baffda196e3f5f1aa
jonathansoufer <jonathanferreira23@gmail.com>
2022-03-09 15:35:43 +0000

    Add notification module & CRUD on users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Post, Get, OnModuleInit } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  OnModuleInit,
+  Param,
+  Body,
+  Delete,
+  Patch,
+  Put,
+} from '@nestjs/common';
 import { Client, ClientKafka, Transport } from '@nestjs/microservices';
 import { ApiBody } from '@nestjs/swagger';
 import { Observable } from 'rxjs';
-import { UserEntity } from './database/user.entity';
+import { User } from './interfaces/user.interface';
 import { UserDto } from './dtos/users.dto';
-import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController implements OnModuleInit {
@@ -24,7 +33,7 @@ export class UsersController implements OnModuleInit {
   private client: ClientKafka;
 
   async onModuleInit() {
-    const requestPatters = ['find-all-users', 'create-user'];
+    const requestPatters = ['find-all-users', 'find-user', 'create-user'];
 
     requestPatters.forEach(
       async (pattern) => this.client.subscribeToResponseOf(pattern),
@@ -32,16 +41,51 @@ export class UsersController implements OnModuleInit {
     );
   }
 
-  constructor(private readonly usersService: UsersService) {}
-
   @Get()
-  index(): Observable<UserEntity[]> {
+  index(): Observable<User[]> {
     return this.client.send('find-all-users', {});
+  }
+
+  @Get(':id')
+  find(@Param('id') id: number): Observable<User> {
+    return this.client.send('find-user', { id });
   }
 
   @Post()
   @ApiBody({ type: UserDto })
-  create(user: UserDto): Observable<UserEntity> {
+  create(user: UserDto): Observable<User> {
     return this.client.send('create-user', user);
+  }
+
+  @Put(':id')
+  @ApiBody({ type: UserDto })
+  update(
+    @Param('id') id: number,
+    @Body() { name, email, phone, password }: UserDto,
+  ) {
+    const payload = {
+      id,
+      name,
+      email,
+      phone,
+      password,
+    };
+
+    return this.client.emit('update-user', payload);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.client.emit('delete-user', { id });
+  }
+
+  @Patch(':id/activate')
+  activate(@Param('id') id: number) {
+    return this.client.emit('activate-user', { id });
+  }
+
+  @Patch(':id/inactivate')
+  inactivate(@Param('id') id: number) {
+    return this.client.emit('inactivate-user', { id });
   }
 }
